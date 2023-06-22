4914e7871a1407d8be066474175b0f08246249c1
Giovaniavs <gavs2@cin.ufpe.br>
2023-06-22 18:21:50 -0300

    Create the home page with the generateRandomColor function working properly

---
+++ lib/home_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+/// Home page of the app where the background color changer will be displayed.
+class HomePage extends StatefulWidget {
+  /// Creates the parameter caller for the HomePage Widget.
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Color _backgroundColor = generateRandomColor();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: InkWell(
+        onTap: () {
+          generateRandomColor();
+          setState(() {
+            _backgroundColor = generateRandomColor();
+          });
+        },
+        child: ColoredBox(
+          color: _backgroundColor,
+          child: const Center(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/// Function responsible for generate a random color with RGB values.
+Color generateRandomColor() {
+  const int limitRgbValue = 255;
+  const double opacityConstantValue = 0.5;
+
+  int redSpectrumValue = 0;
+  int greenSpectrumValue = 0;
+  int blueSpectrumValue = 0;
+
+  redSpectrumValue = generateRandomNumberBetween(0, limitRgbValue);
+  greenSpectrumValue = generateRandomNumberBetween(0, limitRgbValue);
+  blueSpectrumValue = generateRandomNumberBetween(0, limitRgbValue);
+
+  return Color.fromRGBO(
+    redSpectrumValue,
+    greenSpectrumValue,
+    blueSpectrumValue,
+    opacityConstantValue,
+  );
+}
+
+/// Function responsible for generate a random number between two values.
+int generateRandomNumberBetween(int initialValue, int limitValue) {
+  final List<int> listOfLoadedNumbers = [];
+
+  for (int i = initialValue; i < limitValue; i++) {
+    listOfLoadedNumbers.add(i);
+  }
+
+  listOfLoadedNumbers.shuffle();
+
+  return listOfLoadedNumbers.first;
+}
